942869d14fbc18989094c0e4881371d2fe00e322
mahdi <0059sharifloo@gmail.com>
2023-05-06 15:34:13 +0330

    docker file and postgre db added

---
+++ AttendanceTest/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AttendanceTest;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}

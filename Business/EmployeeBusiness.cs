196814afd162a7ecb9d24f15fea5ec2de3c65fdf
mahdi <0059sharifloo@gmail.com>
2023-05-06 14:14:31 +0330

    project builded

---
+++ Business/EmployeeBusiness.cs
@@ -0,0 +1,21 @@
+﻿using Attendance.Entity;
+
+namespace Attendance.Business
+{
+    public class EmployeeBusiness
+    {
+        private List<Employee> employees = new List<Employee>();
+
+        public EmployeeBusiness()
+        {
+            employees.Add(new Employee() { Age = 2 , Id = 1 , FirstName = "reza"});
+            employees.Add(new Employee() { Age = 2 , Id = 1 , FirstName = "mehdi"});
+
+        }
+
+        public List<Employee> Employees()
+        {
+            return employees;
+        }
+    }
+}
---
+++ Entity/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace Attendance.Entity
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailId { get; set; }
+
+        //public List<Activity> Activities { get; set; }
+    }
+}
